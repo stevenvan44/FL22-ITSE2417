6eda35859720e8fa5d1ad87f576dcf5a349df88b
stevenvan44 <120118226+stevenvan44@users.noreply.github.com>
2022-12-13 10:34:10 -0600

    Add files via upload

--- Vanslyke.java
+++ Vanslyke.java
@@ -5,7 +5,7 @@ import java.util.Random;
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-class Vanslyke {
+class Vanslyke implements FinalProject{
 
     public Vanslyke() {
 
